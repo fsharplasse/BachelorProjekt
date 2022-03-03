1aa84f5b836a0108911b3ec19d8215cc6107a081
fsharplasse <55539585+fsharplasse@users.noreply.github.com>
2022-03-03 10:50:38 +0100

    Update bachelor.R

--- bachelor.R
+++ bachelor.R
@@ -18,30 +18,30 @@ colnames(choice)[10] <- "Prices"
 colnames(choice)[7] <- "FuelSize"
 colnames(choice)[1] <- "MakeYear"
 
-choice_sub <- choice %>% group_by(Year, Fuel) %>% summarise(AverageWeight = mean(Weight))
+choice_sub <- choice %>% group_by(MakeYear, Fuel) %>% summarise(AverageWeight = mean(Weight))
 
-ggplot(choice_sub,aes(x=Year,y=AverageWeight,color=Fuel))+
+ggplot(choice_sub,aes(x=MakeYear,y=AverageWeight,color=Fuel))+
   xlab("Registration Year") +
   ylab("Weight in Kg")+
   geom_line()
 
-choice_sub1 <- choice %>% group_by(Year, Fuel) %>% summarise(AverageSize = mean(Size))
+choice_sub1 <- choice %>% group_by(MakeYear, Fuel) %>% summarise(AverageSize = mean(Size))
 
-ggplot(choice_sub1,aes(x=Year,y=AverageSize,color=Fuel))+
+ggplot(choice_sub1,aes(x=MakeYear,y=AverageSize,color=Fuel))+
   xlab("Registration Year") +
   ylab("Size in cubic metres")+
   geom_line()
 
-choice_sub2 <- choice %>% group_by(Year, Fuel) %>% summarise(AverageEE = mean(EngineEffect))
+choice_sub2 <- choice %>% group_by(MakeYear, Fuel) %>% summarise(AverageEE = mean(EngineEffect))
 
-ggplot(choice_sub2,aes(x=Year,y=AverageEE,color=Fuel))+
+ggplot(choice_sub2,aes(x=MakeYear,y=AverageEE,color=Fuel))+
   xlab("Registration Year") +
   ylab("Engine Effect (kW)")+
   geom_line()
 
-choice_sub3 <- choice %>% group_by(Year, FuelSize) %>% summarise(AveragePrice = mean(Prices))
+choice_sub3 <- choice %>% group_by(MakeYear, FuelSize) %>% summarise(AveragePrice = mean(Prices))
 
-ggplot(choice_sub3,aes(x=Year,y=AveragePrice,color=FuelSize))+
+ggplot(choice_sub3,aes(x=MakeYear,y=AveragePrice,color=FuelSize))+
   xlab("Registration Year") +
   ylab("Price")+
   geom_line()
