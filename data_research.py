49abb57018851bd7a8fdd1004d14900b10db9de5
Nicholai1 <nicholaihjelme@gmail.com>
2022-03-24 11:42:26 +0100

    update

--- data_research.py
+++ data_research.py
@@ -46,7 +46,7 @@ combined_data['kmL'] = pd.to_numeric(combined_data['kmL'], errors = 'coerce')
 combined_data['kmL'] = combined_data['kmL'].astype(float)
 combined_data = combined_data.reset_index(drop = True)
 print("\ncombined_data after drop duplicates:\n", combined_data)
-combined_data.to_excel(path + 'final_data.xlsx')
+combined_data.to_csv(path + 'final_datacsv.csv')
 
 
 # MAKING FUEL EFFICIENCY PLOT
